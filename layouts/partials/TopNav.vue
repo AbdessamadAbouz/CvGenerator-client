e7cac07e60ac311045ae3f417234fe974fea429d
Abdessamad Abouzaid <abdessamad.abouz@gmail.com>
2020-06-08 20:34:55 +0100

    First commit

---
+++ layouts/partials/TopNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-navbar type="light" variant="light">
+        <nuxt-link class='navbar-brand' to="/">CvGenerator</nuxt-link>
+        <div class="container">
+
+        <!-- Navbar dropdowns -->
+        <b-navbar-nav>
+          <li class="nav-item">
+            <nuxt-link class='nav-link' to="/dashboard">Dashboard</nuxt-link>
+          </li>
+        </b-navbar-nav>
+
+
+        <b-navbar-nav class='ml-auto'>
+          <li class="nav-item">
+            <nuxt-link class='nav-link' to="/auth/login">Login</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class='nav-link' to="/auth/register">Register</nuxt-link>
+          </li>
+        </b-navbar-nav>
+      </div>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/auth/register.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container">
+    <div class="col-md-6 offset-md-3">
+      <div class="card mt-4">
+        <div class="card-header">
+          <p class='mb-0'>Register</p>
+        </div>
+        <div class="card-body">
+          <form @submit.prevent="Register">
+            <div class="form-group">
+              <label>Name</label>
+              <input v-model="form.name" type="text" class="form-control" placeholder="Name">
+            </div>
+            <div class="form-group">
+              <label>Email</label>
+              <input v-model="form.email" type="email" class="form-control" placeholder="Email">
+            </div>
+            <div class="form-group">
+              <label>Password</label>
+              <input v-model="form.password" type="password" class="form-control" placeholder="Password">
+            </div>
+            <div class="form-group">
+              <label>Confirm password</label>
+              <input v-model="form.password_confirmation" type="password" class="form-control" placeholder="Confirm your Password">
+            </div>
+            <div class="form-group">
+              <input type="submit" value="Register" class="btn btn-default w-100">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+      }
+    }
+  },
+  methods: {
+    async Register() {
+      await this.$axios.post('/auth/register', this.form);
+
+      this.$auth.login({data: this.form});
+
+      this.$auth.push({name: 'index'});
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
