ce5f193121a6771a2159860fd82e789f12ad1629
Abdessamad Abouzaid <abdessamad.abouz@gmail.com>
2020-06-10 20:05:58 +0100

    updated on languages

--- pages/user/languages.vue
+++ pages/user/languages.vue
@@ -32,8 +32,35 @@
         </div>
       </div>
     </div>
-    <div class="col-md-4 offset-md-4 mt-4" v-if="langues">
-      <b-table striped hover :items="langues.data.langues" :fields="fields"></b-table>
+    <div class="col-md-8 offset-md-2 mt-4" v-if="langues">
+      <table class="table table-sm">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Language label</th>
+      <th scope="col">Level</th>
+      <th scope="col">Edit/Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    <template v-for="langue in langues.data.langues">
+    <tr :key="langue.id">
+      <th scope="row">{{langue.id}}</th>
+      <td>{{langue.label}}</td>
+      <td>{{langue.level}}</td>
+      <td>
+        <a href="#" @click="editLangue(langue)">
+          Edit
+        </a>
+        /
+        <a href="#" @click="deleteLangue(langue.id)">
+            Delete
+        </a></td>
+    </tr>
+    </template>
+  </tbody>
+</table>
     </div>
 
   </div>
@@ -68,16 +95,32 @@ export default {
     async store() {
       try {
         this.$axios.post('/user/langues',this.form);
-        this.$axios.get('/user/langues').then(response => (this.langues = response));
+        this.getlanguages();
         this.message = 'stored succesfully';
       } catch(e) {
         this.message = e;
       }
 
+    },
+    async getlanguages() {
+      this.$axios.get('/user/langues').then(response => (this.langues = response));
+    },
+    deleteLangue(id) {
+      this.$axios.delete('user/langues/'+id).then(response => {
+          this.message = response.data.message;
+          this.getlanguages();
+      }).catch(e=> {
+          this.message = e;
+      });
+
+    },
+    editLangue($langue) {
+
     }
   },
-  mounted () {
-    this.$axios.get('/user/langues').then(response => (this.langues = response));
+  created() {
+    this.getlanguages();
+    // setInterval(() => this.getlanguages(),10000);
   }
 }
 </script>
